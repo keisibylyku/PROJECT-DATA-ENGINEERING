fafe1de9dabcbff6796e758fe7b7a194a3e390d2
keisibylyku <keisibylyku@unyt.edu.al>
2024-10-07 04:15:28 -0700

    Update charts.py

--- charts/charts.py
+++ charts/charts.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.write('Hello World!')
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from collections import Counter
+
+# Ignore warnings
+warnings.filterwarnings('ignore')
+
+# Set Seaborn style
+sns.set(style='whitegrid')
+
+# Title for the Streamlit app
+st.title("GOLD LAYER REPORTS FOR MOVIES")
